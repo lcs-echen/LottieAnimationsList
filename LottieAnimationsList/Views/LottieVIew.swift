356657d40678211ba1cdee3316a29d4997ff95cb
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-01-28 10:17:54 -0500

    added several animations that I like.

---
+++ LottieAnimationsList/Views/LottieVIew.swift
@@ -0,0 +1,20 @@
+//
+//  LottieVIew.swift
+//  LottieAnimationsList
+//
+//  Created by Evelyn Chen on 2023-01-28.
+//
+
+import SwiftUI
+
+struct LottieVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LottieVIew_Previews: PreviewProvider {
+    static var previews: some View {
+        LottieVIew()
+    }
+}

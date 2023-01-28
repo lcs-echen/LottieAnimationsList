fa62e0fe39d2af19e85ec09be38a633a951ab6ee
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-01-28 09:49:22 -0500

    Initial Commit

---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by Evelyn Chen on 2023-01-28.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
